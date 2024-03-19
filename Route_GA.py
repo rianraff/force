4e8ee922473058a9722389b0851f47399914a2d2
rianraff <aldrian.raffi@ui.ac.id>
2024-03-19 13:46:12 +0700

    fix download path

--- Route_GA.py
+++ Route_GA.py
@@ -48,12 +48,14 @@ def getRequestApproval(token, startCreatedDate, endCreatedDate):
         'Cookie': '__cf_bm=cXzQEE9tr05sjFIWOFtoB033onNNsMjiuuOfs92P2Ew-1710736596-1.0.1.1-e0AI1WbIvwm7.V5KjVXQjilLFh_XX0oxEexOmx8WoYOvgI0RGcsP_bSTwNCTSdRIloXrSDhbZSuiku092ohiSw'
     }
 
+    base_dir = os.path.join(os.getcwd(), "Input")
+
     response = requests.get(url, params=params, headers=headers)
     if response.status_code == 200:
         json_response = json.loads(response.text)
         contents = json_response["data"]["content"]
         for content in contents:
-            os.makedirs(os.path.join(os.getcwd(), content["description"]))
+            os.makedirs(os.path.join(base_dir, content["description"]))
             getDetailRequest(token, content["id"], content["description"])
             
     else:
@@ -83,20 +85,20 @@ def getDetailRequest(token, id, cluster_id):
 
 def downloadReqApproval(token, cluster_id, file_name, minioId):
     url = f'https://www.getapprove.xl.co.id/api/v2/bpmn/requestDocument/download-file?minioId={minioId}'
-  # Replace with the URL of the file you want to download
+    # Replace with the URL of the file you want to download
     filename = os.path.join(cluster_id, file_name)  # Specify the filename for the downloaded file
     headers = {
         'User-ID': 'idsta.harynr@xl.co.id',
         'Authorization': token,
     }
-    file_path = os.getcwd()
+    base_dir = os.path.join(os.getcwd(), "Input")
     # Send a GET request to the URL
     response = requests.get(url, headers=headers)
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
         # Open the file in binary write mode
-        with open(os.path.join(file_path,filename), 'wb') as f:
+        with open(os.path.join(base_dir,filename), 'wb') as f:
             # Write the content of the response to the file
             f.write(response.content)
         print(f"File '{filename}' downloaded successfully.")
